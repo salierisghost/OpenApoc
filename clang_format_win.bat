1f75d94c8cff1fa9633f73d3d93f8dcc19109ded
SupSuper <supsuper@gmail.com>
2017-12-17 21:39:48 +0000

    Add tests to clang-format

--- clang_format_win.bat
+++ clang_format_win.bat
@@ -1,19 +1,12 @@
-for /R forms %%f in (*.cpp,*.h) do clang-format -i "%%f"
-for /R framework %%f in (*.cpp,*.h) do clang-format -i "%%f"
-for /R game %%f in (*.cpp,*.h) do clang-format -i "%%f"
-for /R library %%f in (*.cpp,*.h) do clang-format -i "%%f"
-for /R tools %%f in (*.cpp,*.h) do clang-format -i "%%f"
+@echo off
 
-for /R forms %%f in (*.cpp,*.h) do clang-format -i "%%f"
-for /R framework %%f in (*.cpp,*.h) do clang-format -i "%%f"
-for /R game %%f in (*.cpp,*.h) do clang-format -i "%%f"
-for /R library %%f in (*.cpp,*.h) do clang-format -i "%%f"
-for /R tools %%f in (*.cpp,*.h) do clang-format -i "%%f"
-
-for /R forms %%f in (*.cpp,*.h) do clang-format -i "%%f"
-for /R framework %%f in (*.cpp,*.h) do clang-format -i "%%f"
-for /R game %%f in (*.cpp,*.h) do clang-format -i "%%f"
-for /R library %%f in (*.cpp,*.h) do clang-format -i "%%f"
-for /R tools %%f in (*.cpp,*.h) do clang-format -i "%%f"
+for /L %%x in (1, 1, 2) do (
+	for /R forms %%f in (*.cpp,*.h) do clang-format -i "%%f"
+	for /R framework %%f in (*.cpp,*.h) do clang-format -i "%%f"
+	for /R game %%f in (*.cpp,*.h) do clang-format -i "%%f"
+	for /R library %%f in (*.cpp,*.h) do clang-format -i "%%f"
+	for /R tests %%f in (*.cpp,*.h) do clang-format -i "%%f"
+	for /R tools %%f in (*.cpp,*.h) do clang-format -i "%%f"
+)
 
 del *.TMP /F /S
