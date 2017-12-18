11bbb66259ec47860a7a067f850b64a68b6e80f9
SupSuper <supsuper@gmail.com>
2017-12-18 00:20:33 +0000

    Check for errors in clang-format

--- clang_format_win.bat
+++ clang_format_win.bat
@@ -1,5 +1,7 @@
-@echo off
+clang-format -i version.h
+if %ERRORLEVEL% NEQ 0 GOTO :error
 
+@echo off
 for /L %%x in (1, 1, 2) do (
 	for /R forms %%f in (*.cpp,*.h) do clang-format -i "%%f"
 	for /R framework %%f in (*.cpp,*.h) do clang-format -i "%%f"
@@ -8,5 +10,11 @@ for /L %%x in (1, 1, 2) do (
 	for /R tests %%f in (*.cpp,*.h) do clang-format -i "%%f"
 	for /R tools %%f in (*.cpp,*.h) do clang-format -i "%%f"
 )
-
 del *.TMP /F /S
+GOTO :end
+
+:error
+echo "clang-format not in path"
+GOTO :end
+
+:end
